a9d628f3ae0b98f86fca2bb9d94b05f56165e0d6
Mayki <aw8hurtz@gmail.com>
2024-02-29 08:55:18 +0800

    Material UI

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material"
+
+
+
+const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">This is h1</Typography>
+            <Typography variant="h2">This is h1</Typography>
+            <Typography variant="h3">This is h1</Typography>
+            <Typography variant="h4">This is h1</Typography>
+            <Typography variant="h5">This is h1</Typography>
+            <Typography variant="h6">This is h1</Typography>
+        </div>
+    )
+}
+
+export default MuiTypography
