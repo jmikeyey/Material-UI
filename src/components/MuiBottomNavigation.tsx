0b1679e2cdb6cd3fde75c4a43b8f88d88a7dcdc6
Mayki <aw8hurtz@gmail.com>
2024-03-01 09:26:04 +0800

    Content Based Bottom Nav

--- src/components/MuiBottomNavigation.tsx
+++ src/components/MuiBottomNavigation.tsx
@@ -3,22 +3,45 @@ import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import React, { useState } from "react";
 
 const MuiBottomNavigation = () => {
+    const [value, setValue] = useState(0);
 
-    const [value, setValue] = useState(0)
-    const handleChange = (e: React.ChangeEvent<HTMLButtonElement>, newValue: number) => {
-        setValue(newValue)
-    }
+    const handleChange = (e: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const getContent = () => {
+        switch (value) {
+            case 0:
+                return <div>Home Content</div>;
+            case 1:
+                return <div>Favorite Content</div>;
+            case 2:
+                return <div>Person Content</div>;
+            default:
+                return null;
+        }
+    };
+
     return (
-        <BottomNavigation
-            sx={{ width: "100%", position: "absolute", bottom: 0, display: 'flex', justifyContent: 'space-around' }}
-            value={value}
-            onChange={(e, newValue) => {setValue(newValue)}}
-            showLabels
-        >
-            <BottomNavigationAction label='Home' icon={<Home />} />
-            <BottomNavigationAction label='Favorite' icon={<Favorite />} />
-            <BottomNavigationAction label='Person' icon={<Person />} />
-        </BottomNavigation>
+        <>
+            {getContent()}
+            <BottomNavigation
+                sx={{
+                    width: "100%",
+                    position: "absolute",
+                    bottom: 0,
+                    display: "flex",
+                    justifyContent: "space-around",
+                }}
+                value={value}
+                onChange={handleChange}
+                showLabels
+            >
+                <BottomNavigationAction label="Home" icon={<Home />} />
+                <BottomNavigationAction label="Favorite" icon={<Favorite />} />
+                <BottomNavigationAction label="Person" icon={<Person />} />
+            </BottomNavigation>
+        </>
     );
 };
 
